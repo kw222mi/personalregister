cb28fdf97613b626dd20aa17d0bb71d552d23227
Therese Weidenstedt <kw222mi@student.lnu.se>
2025-11-04 14:58:25 +0100

    add attributes and methodes to the classes

--- personalregister/Employee.cs
+++ personalregister/Employee.cs
@@ -8,5 +8,21 @@ namespace personalregister
 {
     internal class Employee
     {
+
+        public String firstName;
+        public String lastName;
+        public decimal salary;
+
+        public Employee(string firstName, string lastName, decimal salary)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.salary = salary;
+        }
+
+        public void employeeToString()
+        {
+            Console.WriteLine(" employee");
+        }
     }
 }
--- personalregister/PersonnelRegister.cs
+++ personalregister/PersonnelRegister.cs
@@ -8,5 +8,15 @@ namespace personalregister
 {
     internal class PersonnelRegister
     {
+        private readonly List<Employee> _employees = new();
+
+        public void Add(Employee e) => _employees.Add(e);
+
+        public IEnumerable<Employee> All() => _employees;
+
+        public void PrintAll()
+        {
+
+        }
     }
 }
